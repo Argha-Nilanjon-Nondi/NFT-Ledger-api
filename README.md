9b8466ce9fc0dc16b752c57a9bef274a78c4f233
Argha Nilanjon Nondi <pcic095@gmail.com>
2021-10-07 22:32:08 +0600

    Add documentation and fix check_signature()

---
+++ README.md
@@ -0,0 +1,144 @@
+
+# Story Blockchain
+
+In this project , I have implemented blockchain technology with python. Here the data can not be changed or modify.So the database file can be share with anyone. If the person changes the database , we can detect it very easily.
+
+## Installation
+Install python 3.6 or higher version.
+And run this command
+```bash
+  pip install pycryptodome
+```
+
+  
+## Documentation
+### Add a writer
+
+#### code
+```python
+from lib.Writer import Writer
+
+objWriter=Writer()
+print(objWriter.add(name="Argha Nilanjon Nondi",nId="12992749084",description="New member",level=3,char="0"))
+```
+#### if the level=3 and char="0" then the hash will start with 000.The time to add a new writer will depends on the level . If the level is increased , the time will be increased as well.
+
+#### output
+```json
+{
+   'private_key': '2d2d2d2d2d424547494e205253412050524956415445204b45592d2d2d2d2d0a4d4949435851494241414b42675143764a57786177......', 
+   'public_key': '2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d494......'
+}
+```
+
+#### Here public_key is saved on ledger.sqlite but the private_key is not saved on ledger.sqlite. A user must save it on their computer and they should not share it with anyone but public_key can be shared with anyone
+
+### Verify the Writers chain
+#### code
+```python
+from lib.Writer import Writer
+
+objWriter=Writer()
+print(objWriter.verify())
+```
+#### output 1
+```boolean
+True
+```
+#### If anybody does not modify the data of the table named 'Writers' of 'Ledger.sqlite' 
+
+#### output 2
+```boolean
+False
+```
+#### If anybody modifies the data of the table named 'Writers' of 'Ledger.sqlite'
+
+
+
+### Add a story
+#### code
+```python
+from lib.Story import Story
+
+privateKey="""2d2d2d2d2d424547494e205253412050524956415445204b45592d2d2d..."""
+objStory=Story()
+print(objStory.add(privateKey=privateKey,storyName="Doctor Sleep",storyDescription="A horror thriller book",level=3,char="0"))
+```
+#### if the level=3 and char="0" then the hash will start with 000.The time to add a new writer will depends on the level . If the level is increased , the time will be increased as well. Here hash is not the signature.
+#### output
+  ```text
+7011f882ad281ec19f300a4c9017961338f48ecacdcbb4b370dda34eaae5e9601......
+```
+####  It is the signature of a story and it can be shared with anyone.
+
+
+
+### Verify the Stories chain
+#### code
+```python
+from lib.Story import Story
+
+objWriter=Story()
+print(objWriter.verify())
+```
+#### output 1
+```boolean
+True
+```
+#### If anybody does not modify the data of the table named 'Stories' of 'Ledger.sqlite' 
+
+#### output 2
+```boolean
+False
+```
+#### If anybody modifies the data of the table named 'Stories' of 'Ledger.sqlite
+
+### Verify a signature
+#### code
+```python
+from lib.Story import Story
+
+signature="5f9dc207c0577846a40cf5c4fecb4b781d7980482e4b6fa62cfb63514be01d581535d59da29a....."
+
+publicKey="""2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d4947664d413047......."""
+
+objStory=Story()
+print(objStory.check_signature(signature=signature,publicKey=publicKey))
+```
+#### output 1
+```boolean
+True
+```
+#### If anybody does not modify the data of the table named 'Stories' of 'Ledger.sqlite'  or the signature is connected to public key
+#### output 2
+```boolean
+False
+```
+ If anybody modifies the data of the table named 'Stories' of 'Ledger.sqlite or the signature is not connected to public key
+
+## Built with
+
+ - python
+ - sql
+ - sqlite
+- blockchain
+- cryptography
+
+  
+## Note
+- If you open the 'Ledger.sqlite' , you can not find the private key . So the database is a public ledger and it can shared with anyone.
+
+- The level argument indeicates the hash complexity . The level argument is an integer type and it has the range of 1 to 64.
+
+- You can change the 'getHash(data=,num=)' function in 'SecretCrypto/__init__.py'  so that  the hacker can not guess how hashes are being generated.
+
+- I said earlier that you can share the 'Ledger.sqlite' with anyone but I will suggest that do not share the python scripts.
+
+- You can use the pattern in other scenarios. Like storing hash of files .
+  
+## License
+- You can use it for educational purpose
+- You can not claim it
+- You can not use it for commercial purpose without my permission
+
+  
--- lib/Story/__init__.py
+++ lib/Story/__init__.py
@@ -7,7 +7,7 @@ def currentTime():
 	date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 	return date_time
 
-class Book:
+class Story:
 	
 	def verify(self):
 			
@@ -15,7 +15,7 @@ class Book:
 		objSql=SqlRunner("Ledger.sqlite")
 		
 		objSql.sql="""
-		  Select PublicKey,BookName,EventDate,Description,Signature,PreviousHash,Char,Level,Token From Books;
+		  Select PublicKey,StoryName,StoryDescription,EventDate,Signature,PreviousHash,Char,Level,Token From Stories;
 		"""
 		
 		data_list=objSql.run()
@@ -23,10 +23,10 @@ class Book:
 		for data in data_list:
 			
 			publicKey=data[0]
-			bookName=data[1]
-			eventDate=data[2]
-			description=data[3]
-			signature=data[4]
+			storyName=data[1]
+			storyDescription=data[2]
+			eventDate=data[3]
+			signature=data[4]	
 			previousHash=data[5]
 			char=data[6]
 			level=data[7]
@@ -34,9 +34,9 @@ class Book:
 			
 			dict_data={
 			"public_key":publicKey,
-			"book_name":bookName,
+			"story_name":storyName,
+			"story_description":storyDescription,
 			"event_date":eventDate,
-			"description":description,
 			"previous_hash":previousHash,
 			"signature":signature,
 			"char":char,
@@ -52,7 +52,7 @@ class Book:
 		return True
 		
 	
-	def add(self,privateKey,bookName,description,level,char):
+	def add(self,privateKey,storyName,storyDescription,level,char):
 		
 		obj=Cryptography()
 		publicKey=obj.privateKeyToPublicKey(privateKey)
@@ -62,7 +62,7 @@ class Book:
 		eventDate=currentTime()
 		
 		objSql.sql="""
-		  Select Hash,No from Books
+		  Select Hash,No from Stories
 		  ORDER BY No DESC LIMIT 1 
 		"""
 		
@@ -71,36 +71,36 @@ class Book:
 		
 		signature_data={
 			"public_key":publicKey,
-			"book_name":bookName,
+			"story_name":storyName,
+			"story_description":storyDescription,
 			"event_date":eventDate,
-			"description":description,
 			"previous_hash":previousHash,
 			"char":char,
 			"level":level
-		}
+			}
 		
 		signature=obj.signData(privateKey,signature_data)
 		
 		dict_data={
 			"public_key":publicKey,
-			"book_name":bookName,
+			"story_name":storyName,
+			"story_description":storyDescription,
 			"event_date":eventDate,
-			"description":description,
 			"previous_hash":previousHash,
 			"signature":signature,
 			"char":char,
 			"level":level
-		}
+			}
 		
 		hash_token=obj.getTokenOfHash(dict_data,char,level)
 		hash=hash_token["hash"]
 		token=hash_token["token"]
 		
 		sql_code="""
-		INSERT INTO Books(No,PublicKey,BookName,Level,Char,PreviousHash,hash,Token,Signature,EventDate,Description)
+		INSERT INTO Stories(No,PublicKey,StoryName,StoryDescription,Level,Char,PreviousHash,hash,Token,Signature,EventDate)
 		
-		Values({no},'{publicKey}','{bookName}',{level},'{char}','{previousHash}','{hash}',{token},'{signature}','{eventDate}','{description}')
-		""".format(no=no+1,publicKey=publicKey,bookName=bookName,level=level,char=char,previousHash=previousHash,hash=hash,token=token,signature=signature,eventDate=eventDate,description=description)
+		Values({no},'{publicKey}','{storyName}','{storyDescription}',{level},'{char}','{previousHash}','{hash}',{token},'{signature}','{eventDate}')
+		""".format(no=no+1,publicKey=publicKey,level=level,char=char,previousHash=previousHash,hash=hash,token=token,signature=signature,eventDate=eventDate,storyName=storyName,storyDescription=storyDescription)
 		
 		objSql.sql=sql_code
 		objSql.run()
@@ -114,29 +114,28 @@ class Book:
 		objSql=SqlRunner("Ledger.sqlite")
 		
 		objSql.sql="""
-		  Select PublicKey,BookName,EventDate,Description,Signature,PreviousHash,Char,Level From Books Where Signature='{signature}'; 
+		  Select PublicKey,StoryName,StoryDescription,EventDate,PreviousHash,Char,Level From Stories Where Signature='{signature}'; 
 		""".format(signature=signature,publicKey=publicKey)
 		
 		data_list=objSql.run()
 		data=data_list[0]
 		publicKey=data[0]
-		bookName=data[1]
-		eventDate=data[2]
-		description=data[3]
-		signature=data[4]
-		previousHash=data[5]
-		char=data[6]
-		level=data[7]
+		storyName=data[1]
+		storyDescription=data[2]
+		eventDate=data[3]
+		previousHash=data[4]
+		char=data[5]
+		level=data[6]
 		
 		dict_data={
-		"public_key":publicKey,
-		"book_name":bookName,
-		"event_date":eventDate,
-		"description":description,
-		"previous_hash":previousHash,
-		"char":char,
-		"level":level
-		}
+			"public_key":publicKey,
+			"story_name":storyName,
+			"story_description":storyDescription,
+			"event_date":eventDate,
+			"previous_hash":previousHash,
+			"char":char,
+			"level":level
+			}
 		
 		status=obj.verifyData(publicKey=publicKey,data=dict_data,signature=signature)
 		
